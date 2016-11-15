d9eb34e10e17b3a499786bd7bc310ea27a982302
Ghislain Chavanne <gh.chavanne@gmail.com>
2016-11-15 14:47:49 +0100

    all routes ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(name: "menage", taskmanager: "bernard")
+Task.create!(name: "balais", taskmanager: "paul")
